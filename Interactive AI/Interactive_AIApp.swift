4a8ab9f8242e919bd72c48f8523eb9438dc10620
Irham Sahbana <irhamsahbana@gmail.com>
2025-09-06 08:09:43 +0700

    Initial Commit

---
+++ Interactive AI/Interactive_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_AIApp.swift
+//  Interactive AI
+//
+//  Created by Irham Sahbana on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
